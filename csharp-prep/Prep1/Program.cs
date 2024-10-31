d7adfe8d9f489bdf2b35c637935fb8accd5b1cda
masizajustin <mas24002@byui.edu>
2024-10-31 15:48:37 +0200

    prep 1 updated

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your surname? ");
+        string lname = Console.ReadLine();
+
+        Console.Write($"Your name is {lname}, {name} {lname}");
+
     }
 }
